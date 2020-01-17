f496d43b712007bd8d1dad94a7af715dde62aefb
Samuubs <samuel.santiago170@gmail.com>
2020-01-17 12:37:56 -0300

    real time with websockets

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const socketio = require('socket.io')
+const http = require('http')
+const cors = require('cors');
 
 const app = express()
+const server = http.Server(app)
+const io = socketio(server);
 
 mongoose.connect('mongodb://samu:45967812sam@ds263928.mlab.com:63928/twitter_clone', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+app.use((req, res, next) => {
+    req.io = io;
+    return next()
+})
+
+app.use(cors());
 app.use(express.json())
 app.use(routes)
 
