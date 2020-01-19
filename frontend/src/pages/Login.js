61ef09b6f0938f82702d1554c21b1d3a9d4187c8
Samuubs <samuel.santiago170@gmail.com>
2020-01-19 14:49:20 -0300

    handle submit login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import './Login.css'
+import './Login.css';
 
 import twitterLogo from '../assets/twitter.svg'
 
-export default function pages() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!username.length) return;
+    
+    localStorage.setItem('@GoTwitter:username', username);
+    history.push(`/timeline`)
+  }
+
   return (
     <div className="login-wrapper">
         <img src={twitterLogo} alt="Twitter Logo"/>
-        <form>
-          <input placeholder="Nome de usuário"/>
+        <form onSubmit={handleSubmit}>
+          <input 
+            placeholder="Nome de usuário"
+            value={username}
+            onChange={event => setUsername(event.target.value)}
+            />
           <button type="submit">Entrar</button>
         </form>
     </div>
